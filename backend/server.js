0fb1063ec20a76c3c6c729f6214c4bc07fe99edd
Aman Mittal <aman280@outlook.com>
2022-07-17 14:44:20 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const { connectDatabase } = require("./config/database");
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+app.listen(5000 || process.env.PORT, () => {
   console.log(`Server is running at 5000`);
 });
