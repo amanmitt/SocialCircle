421fc59283275986d375ab8409a755ebe741a00f
Aman Mittal <aman280@outlook.com>
2022-07-18 16:54:24 +0530

    make it better

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -47,26 +47,26 @@ const Home = () => {
     <Loader />
   ) : (
     <div className="home">
-      <div className="homeleft">
-        {posts && posts.length > 0 ? (
-          posts.map((post) => (
-            <Post
-              key={post._id}
-              postId={post._id}
-              caption={post.caption}
-              postImage={post.image.url}
-              likes={post.likes}
-              comments={post.comments}
-              ownerImage={post.owner.avatar.url}
-              ownerName={post.owner.name}
-              ownerId={post.owner._id}
-            />
-          ))
-        ) : (
-          <Typography variant="h6">No posts yet</Typography>
-        )}
-      </div>
-      <div className="homeright">
+      {/* <div className="homeleft"> */}
+      {posts && posts.length > 0 ? (
+        posts.map((post) => (
+          <Post
+            key={post._id}
+            postId={post._id}
+            caption={post.caption}
+            postImage={post.image.url}
+            likes={post.likes}
+            comments={post.comments}
+            ownerImage={post.owner.avatar.url}
+            ownerName={post.owner.name}
+            ownerId={post.owner._id}
+          />
+        ))
+      ) : (
+        <Typography variant="h6">No posts yet</Typography>
+      )}
+      {/* </div> */}
+      {/* <div className="homeright">
         {users && users.length > 0 ? (
           users.map((user) => (
             <User
@@ -79,7 +79,7 @@ const Home = () => {
         ) : (
           <Typography>No Users Yet</Typography>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
