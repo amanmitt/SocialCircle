e696ab09cef78983ca14e674bd59b4ae5f02d86d
Aman Mittal <aman280@outlook.com>
2022-07-18 16:29:20 +0530

    make it better

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,13 @@ const dotenv = require("dotenv");
 
 exports.connectDatabase = () => {
   mongoose
-    .connect("mongodb://localhost:27017/SocialCircle")
+    .connect(
+      "mongodb+srv://aman:sBLFEQhM9EEhXQxE@cluster0.dbezb.mongodb.net/SocialCircle?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    )
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
